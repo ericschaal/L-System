2b2e24d61da36158b3ba9e7888ae82ff8b272ed2
Eric Schaal <eric.schaal@mail.mcgill.ca>
2017-03-29 17:34:10 -0400

    Dev

--- FloatParser.cpp
+++ FloatParser.cpp
@@ -16,16 +16,14 @@ using namespace std;
 typedef vector<char>::iterator char_iterator;
 typedef tuple<char_iterator, char_iterator> iterator_tuple;
 
-
-class DfaResult;
+class Result;
 class Compound;
-
-void dfa(int state, vector<char>::iterator begin, vector<char>::iterator end, DfaResult* r, char_iterator lastFloat);
 void generateString(int length);
 void printString(vector<char>* string);
 void process(vector<iterator_tuple> tasks);
 vector<iterator_tuple> divide(int threads);
-void deleteAll(char_iterator from, char_iterator to);
+Result dfa(int state, char_iterator start, char_iterator end);
+void dfa_internal(int state, char_iterator start, char_iterator end, Result* r);
 
 char generator[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', 'x'};
 
@@ -34,35 +32,21 @@ vector<char> *myString;
 regex oneToNine = regex("[1-9]");
 regex zeroToNine = regex("[0-9]");
 
-
-class DfaResult {
+class Result {
 
 public:
-    int state;
-    vector<function<void()>> functions;
-    char_iterator lastFloat;
-
-    void addDelete(char_iterator from, char_iterator to) {
-        function<void()> func = [=]() -> void {
-            deleteAll(from, to);
-        };
-        functions.push_back(func);
-    }
-
-    void exec() {
-        for (auto it = functions.begin(); it != functions.end(); ++it) {
-            (*it)();
-        }
-    }
+    int endState;
+    vector<vector<char>> final;
+    vector<char> current;
 
 };
 
 class Compound{
 
 public:
-    DfaResult results[5];
+    Result results[5];
 
-    void addResult(int index, DfaResult r) {
+    void addResult(int index, Result r) {
         results[index] = r;
         return;
     }
@@ -70,6 +54,85 @@ public:
 
 };
 
+
+
+void dfa_internal(int state, char_iterator start, char_iterator end, Result& r) {
+
+    if (start > end) {
+        r.endState = state;
+        return;
+    }
+
+    switch (state) {
+
+        case 0:
+            if (regex_match(string(1, *start), oneToNine)) {
+                r.current.push_back(*start);
+                return dfa_internal(2, start+1, end, r);
+            }
+            else if (*start == '0') {
+                r.current.push_back(*start);
+            }
+            else {
+                r.current.clear();
+                return dfa_internal(0, start+1, end, r);
+            }
+        case 1:
+            if (*start == '.') {
+                r.current.push_back(*start);
+                dfa_internal(3, start+1, end, r);
+            } else {
+                r.current.clear();
+                return dfa_internal(0, start+1, end, r);
+            }
+        case 2:
+            if (regex_match(string(1,*start), zeroToNine)) {
+                r.current.push_back(*start);
+                return dfa_internal(2, start+1, end, r);
+            } else if (*start == '.') {
+                r.current.push_back(*start);
+                return dfa_internal(3, start+1, end, r);
+            } else {
+                r.current.clear();
+                return dfa_internal(0, start+1, end, r);
+            }
+        case 3:
+            if (regex_match(string(1, *start), zeroToNine)) {
+                r.current.push_back(*start);
+                return dfa_internal(4, start+1, end, r);
+            }
+            else {
+                r.current.clear();
+                return dfa_internal(0, start+1, end, r);
+            }
+        case 4:
+            if (regex_match(string(1, *start), zeroToNine)) {
+                r.current.push_back(*start);
+                return dfa_internal(4, start+1, end, r);
+            }
+            else {
+                r.final.push_back(r.current);
+                r.current.clear();
+                return dfa_internal(0, start+1, end, r);
+            }
+        default:
+            return;
+
+    }
+
+}
+
+Result dfa(int state, char_iterator start, char_iterator end) {
+
+    Result r;
+    dfa_internal(state, start, end, r);
+
+
+
+    return r;
+
+
+}
 
 int main(int argc, char *argv[]) {
 
@@ -90,16 +153,35 @@ int main(int argc, char *argv[]) {
     assert(stringLength >= (threads+1));
 
     omp_set_num_threads(threads); // setting max number of threads
-    myString = new vector<char>(); // string holder
 
+    myString = new vector<char>(); // string holder
     generateString(stringLength); // generate random string
-    //printString(myString); // print original string
+    printString(myString); // print original string
+
     vector<iterator_tuple> tasks = divide(threads+1); // generate tasks
+
+
     double start = omp_get_wtime();
+    //Result r = dfa(0, myString->begin(), myString->end());
     process(tasks);
     double stop = omp_get_wtime();
-    //printString(myString);
+
+//    auto floats = r.final;
+//
+//    for (auto i = floats.begin(); i != floats.end(); ++i) {
+//        for (auto it = (*i).begin(); it != (*i).end(); ++it) {
+//            cout << *it << " ";
+//        }
+//        cout << "\t";
+//    }
+//
+//    cout << endl;
+//
+
+
     printf("Time taken: %.2fs\n", stop-start);
+
+
     return EXIT_SUCCESS;
 }
 
@@ -124,82 +206,6 @@ void generateString(int length) {
 
 
 /**
- * Dfa to check reg exp.
- * @param state start state
- * @param begin start position
- * @param end  end position
- * @param r result pointer
- * @param lastFloat position of last known float
- */
-void dfa(int state, vector<char>::iterator begin, vector<char>::iterator end, DfaResult* r, char_iterator lastFloat) {
-
-    r->state = state;
-    r->lastFloat = lastFloat;
-
-    if (begin > end) {
-        return;
-    }
-
-    switch (state) {
-        case 0:
-            if (regex_match(string(1,*begin), oneToNine))
-                return dfa(2, begin+1, end, r, lastFloat);
-            else if (*begin == '0')
-                return dfa(1, begin+1, end, r, lastFloat);
-            else {
-                r->addDelete(lastFloat+1, begin);
-                return dfa(0, begin+1, end, r, lastFloat);
-            }
-        case 1:
-            if (*begin == '.')
-                return dfa(3, begin+1, end, r, lastFloat);
-            else {
-                r->addDelete(lastFloat+1, begin);
-                return dfa(0, begin+1, end, r, lastFloat);
-            }
-        case 2:
-            if (regex_match(string(1,*begin), zeroToNine))
-                return dfa(2, begin+1, end, r, lastFloat);
-            else if (*begin == '.')
-                return dfa(3, begin+1, end, r, lastFloat);
-            else {
-                r->addDelete(lastFloat+1, begin);
-                return dfa(0, begin+1, end, r, lastFloat);
-            }
-        case 3:
-            if (regex_match(string(1,*begin), zeroToNine)) {
-                return dfa(4, begin + 1, end, r, lastFloat);
-            }
-            else {
-                r->addDelete(lastFloat+1, begin);
-                return dfa(0, begin+1, end, r, lastFloat);
-            }
-        case 4:
-            if (regex_match(string(1,*begin), zeroToNine)) {
-                lastFloat = begin;
-                return dfa(4, begin + 1, end, r, lastFloat);
-            }
-            else {
-                r->addDelete(lastFloat+1, begin);
-                return dfa(0, begin+1, end, r, lastFloat);
-            }
-
-
-    }
-}
-
-/**
- * Replace all characters in range
- * @param from inclusive start iterator
- * @param to inclusive end iterator
- */
-void deleteAll(char_iterator from, char_iterator to) {
-    for (auto it = from; it <= to; it++) {
-        *it = ' ';
-    }
-}
-
-/**
  * Divide string into multiple tasks
  * @param threads
  * @return tuple of a start iterator and end iterator
@@ -217,42 +223,56 @@ vector<iterator_tuple> divide(int threads) {
 }
 
 
-
 void process(vector<iterator_tuple> tasks) {
-    DfaResult result;
+    Result result;
     Compound compounds[tasks.size()-1];
-    #pragma omp parallel
+#pragma omp parallel
     {
 
-        #pragma omp single
+#pragma omp single
         {
 
-            dfa(0, get<0>(*tasks.begin()), get<1>(*tasks.begin()), &result, myString->begin()-1); // start first thread
+            result = dfa(0, get<0>(*tasks.begin()), get<1>(*tasks.begin())); // start first thread
 
         }
-        #pragma omp parallel for schedule(static, 1) collapse(1)
+#pragma omp parallel for schedule(static, 1) collapse(1)
         for (uint i = 1; i < tasks.size(); i++) { // start all other threads, they match on their task starting from all possible dfa state
             {
                 for (int j = 0; j < 5; j++) {
                     auto startIterator = get<0>(tasks[i]);
                     auto endIterator = get<1>(tasks[i]);
-                    DfaResult r = DfaResult();
-                    dfa(j, startIterator, endIterator, &r, myString->end());
+                    Result r = dfa(j, startIterator, endIterator);
                     compounds[i - 1].addResult(j, r);
                 }
             }
         }
 
     }
-    #pragma omp barrier
+#pragma omp barrier
 
-    result.exec(); // remove non float from first task.
-    int lastState = result.state;
+    int lastState = result.endState;
     for (auto compound: compounds) {
         auto resultFromState = compound.results[lastState]; // select the right starting state
-        resultFromState.exec(); // remove non float in string
-        lastState = resultFromState.state; // update state for next task
+
+
+        auto floats = resultFromState.final;
+
+        for (auto i = floats.begin(); i != floats.end(); ++i) {
+            for (auto it = (*i).begin(); it != (*i).end(); ++it) {
+                cout << *it << " ";
+            }
+            cout << "\t";
+        }
+
+        cout << endl;
+
+
+        lastState = resultFromState.endState; // update state for next task
+
+
+
     }
 
 
 }
+
